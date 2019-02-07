740d6a18f2c1ff2da56719c7841561a9bfe219d1
Emilio Pomares <remilia@Emilios-iMac.local>
2019-02-07 13:18:36 +0100

    Initial commit

--- src/Chapter4.cpp
+++ src/Chapter4.cpp
@@ -32,6 +32,20 @@ int main(int argc, char **argv) {
 
 	cout << "\n\n";
 
+	// box
+	cout << "Input size, again: ";
+	getline(cin, line);
+	size = stoi(line);
+
+	for(i = 0; i < size; ++i) {
+		for(j = 0; j < size; ++j) {
+			cout << "* ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n\n";
+
 	// Top to bottom diagonal
 	cout << "Can I haz Input size, pls?: ";
 	getline(cin, line);
@@ -39,7 +53,7 @@ int main(int argc, char **argv) {
 
 	for(i = 0; i < size; ++i) {
 		for(j = 0; j < i; ++j) {
-			cout << " ";
+			cout << "  ";
 		}	
 		cout << "*\n";
 	}
@@ -51,6 +65,116 @@ int main(int argc, char **argv) {
 	getline(cin, line);
 	size = stoi(line);
 
-	for(
+	for(i = 0; i < size; ++i) {
+		for(j = 0; j < (size-i); ++j) {
+			cout << "  ";
+		}
+		cout << "*\n";
+	}
+
+	cout << "\n\n";
+
+	
+	// frame, engineer style
+	cout << "Please bear with me: ";
+	getline(cin, line);
+	size = stoi(line);
+
+	// first line
+	for(j = 0; j < size; ++j) {
+		cout << "* ";
+	}
+	cout << "\n";
+	// middle lines
+	for(i = 1; i < size-1; ++i) {
+		cout << "* ";
+		for(j = 1; j < size-1; ++j) {
+			cout << "  ";
+		}
+		cout << "* \n";
+	}
+	// last line
+	for(j = 0; j < size; ++j) {
+		cout << "* ";
+	}
+
+	cout << "\n\n";
+
+
+	// Checkbox, engineer style
+	cout << "Almost there, I promise: ";
+	getline(cin, line);
+	size = stoi(line);
+
+	bool isOdd = (size % 2 != 0);
+
+	// first line
+	for(j = 0; j < size; ++j) {
+		cout << "* ";
+	}
+	cout << "\n";
+	// top middle lines
+	for(i = 1; i <= (size-1)/2-isOdd; ++i) {
+		// start of row
+		cout << "* ";
+		// spaces
+		for(j = 1; j < i; ++j) {
+                        cout << "  ";
+                }
+		// asterisk	
+		cout << "* ";
+		// moar spaces
+		for(j = 0; j < size-2-i*2; ++j) {
+			cout << "  ";
+		}
+		// second asterisk
+		cout << "* ";
+		// even moar spaces
+                for(j = 1; j < i; ++j) {
+                        cout << "  ";
+                }
+		// end of row
+		cout << "*\n";
+	}
+	if(isOdd) {
+		cout << "* ";
+		for(j = 0; j < size/2-1; ++j) {
+			cout << "  ";
+		}
+		cout << "* ";
+		for(j = 0; j < size/2-1; ++j) {
+			cout << "  ";
+		}	
+		cout << "*\n";
+	}
+ 	// bottom middle lines
+        for(i = 1; i <= (size-1)/2-1; ++i) {
+                // start of row
+                cout << "* ";
+                // spaces
+                for(j = 1; j < (size-2-i*2)/2+isOdd; ++j) {
+                        cout << "  ";
+                }
+                // asterisk
+                cout << "* ";
+                // moar spaces
+                for(j = 0; j < i*2-isOdd; ++j) {
+                        cout << "  ";
+                }
+                // second asterisk
+                cout << "* ";
+                // even moar spaces
+                for(j = 1; j < (size-2-i*2)/2+isOdd; ++j) {
+                        cout << "  ";
+                }
+                // end of row
+                cout << "*\n";
+        }
+	// last line
+	for(j = 0; j < size; ++j) {
+		cout << "* ";
+	}
+
+	cout << "\n\n";
 	
 }
